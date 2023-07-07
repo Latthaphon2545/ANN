496f4a2a07183945391f46b2cd579659498d024c
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2023-07-07 15:45:32 +0700

    1

--- Lab1/p7.py
+++ Lab1/p7.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     c = float(c)
     r = float(r)
     periods = int(input("periods: "))
-    if c*S > r:
+    if (c * S) > r:
         print("--> It is spreading!")
     else:
         print("--> It is contained!")
@@ -19,8 +19,8 @@ if __name__ == "__main__":
         m =  "0: S = {:.0f}; I = {:.0f}; R = {:.0f}\n".format(S, I, R)
         f.write(m)
         for n in range(periods):
-            dS = -c * S * I
-            dI = c * S * I - r * I
+            dS = (-c) * S * I
+            dI = (c * S * I) - (r * I)
             dR = r * I
             S += dS
             I += dI
